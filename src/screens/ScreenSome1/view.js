xiaogliu/react-native-complete-demo

添加react navigation，实现页面间路由跳转

src/screens/ScreenSome1/view.js
@@ -1,0 +1,9 @@
+import React from "react";
+import { View, Button, Text } from "react-native";
+
+export default self => (
+  <View>
+    <Text>home</Text>
+    <Button title="goHome" onPress={() => self.navigation.navigate("ScreenHome")} />
+  </View>
+);

zhangzzqq/reduxStudy1

src/screens/ScreenSome1/view.js
@@ -1,0 +1,9 @@
+import React from "react";
+import { View, Button, Text } from "react-native";
+
+export default self => (
+  <View>
+    <Text>home1</Text>
+    <Button title="goHome2" onPress={() => self.navigation.navigate("ScreenSome2")} />
+  </View>
+);